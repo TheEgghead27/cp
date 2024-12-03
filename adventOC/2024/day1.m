1ccb541ad0bc3476bca1950b7f4b01e0699c1c33
David Chen <dchen07@protonmail.com>
2024-12-02 21:08:24 -0500

    Octave funny moments #1

---
+++ adventOC/2024/day1.m
@@ -0,0 +1,10 @@
+A = csvread("1/A");
+sum(abs(sort(A(:,2)) - sort(A(:,1))))
+
+s = 0;
+for i = 1:length(A(:,1))
+  %printf("%d checking %d, A(%d,1)\n", s, A(i,1), i);
+  s += sum(A(:,2) == A(i,1)) * A(i,1);
+end
+printf("s = %d\n", s);
+% s > 391
